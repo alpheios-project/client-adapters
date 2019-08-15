14a6a8b7d4d3b91823e213664d37109fc0850b10
Bridget Almas <balmsa@gmail.com>
2019-08-15 11:16:49 -0400

    make console messages consistent per code review

--- src/adapters/base-adapter.js
+++ src/adapters/base-adapter.js
@@ -155,15 +155,15 @@ class BaseAdapter {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
-      console.warn('Unexpected response retrieving data from an Alpheios service', error)
+      console.error('Alpheios error: unexpected response retrieving data from service', error)
     } else if (error.request) {
       // The request was made but no response was received
       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
       // http.ClientRequest in node.js
-      console.warn('No response from an Alpheios service', error)
+      console.error('Alpheios error: no response from service', error)
     } else {
       // Something happened in setting up the request that triggered an Error
-      console.warn('Unexpected error requesting data from an Alpheios service', error.message)
+      console.error('Alpheios error: unexpected error requesting data from service', error.message)
     }
   }
 
