e3ed40fee1b3f6e596cd9255aa5212f7e13c4e94
Irina <irina060981@yandex.ru>
2019-10-28 21:55:07 +1000

    add chineseAdapter - step1

---
+++ src/adapters/chineseloc/adapter.js
@@ -0,0 +1,94 @@
+/* eslint-disable no-unused-vars */
+import BaseAdapter from '@/adapters/base-adapter'
+import { ChineseLanguageModel, Lemma, Lexeme, Homonym, Feature, Definition } from 'alpheios-data-models'
+
+class AlpheiosChineseLocAdapter extends BaseAdapter {
+  constructor (config = {}) {
+    super()
+    this.config = {
+      category: 'morphology',
+      adapterName: 'chineseloc',
+      method: 'getHomonym'
+    }
+  }
+
+  get languageID () { return ChineseLanguageModel.languageID }
+
+  fetchChineseData (targetWord) {
+    return {}
+  }
+
+  getHomonym (targetWord) {
+    try {
+      const res = this.fetchChineseData(targetWord)
+      if (res) {
+        let homonym = this.transformData(res, targetWord)
+
+        if (!homonym) {
+          this.addError(this.l10n.messages['MORPH_TUFTS_NO_HOMONYM'].get(targetWord, this.languageID.toString()))
+          return
+        }
+        return homonym
+      }
+    } catch (error) {
+      this.addError(this.l10n.messages['MORPH_TUFTS_UNKNOWN_ERROR'].get(error.mesage))
+    }
+  }
+
+  transformData (rawLexemes, targetWord) {
+    let lexemes = []
+    rawLexemes.forEach(rawLexeme => {
+      let lemma = new Lemma(rawLexeme.dictEntry, this.languageID)
+
+      let features = this.extractFeatures(rawLexeme)
+      lemma.addFeatures(features)
+
+      let shortdefs = this.extractShortDefinitions(rawLexeme)
+
+      let lexmodel = new Lexeme(lemma, [])
+      lexmodel.meaning.appendShortDefs(shortdefs)
+
+      lexemes.push(lexmodel)
+    })
+
+    if (lexemes.length > 0) {
+      return new Homonym(lexemes, targetWord)
+    } else {
+      return undefined
+    }
+  }
+
+  extractFeatures (rawLexeme) {
+    let featuresArr = [
+      { checkAttribute: 'pinyin', method: this.defineSimpleFeature.bind(this), featureType: Feature.types.pronunciation },
+      { checkAttribute: 'format', method: this.defineSimpleFeature.bind(this), featureType: Feature.types.dialect },
+      { checkAttribute: 'mandarin', method: this.defineSimpleFeature.bind(this), featureType: Feature.types.mandarin },
+      { checkAttribute: 'cantonese', method: this.defineSimpleFeature.bind(this), featureType: Feature.types.cantonese },
+      { checkAttribute: 'tang', method: this.defineSimpleFeature.bind(this), featureType: Feature.types.tang },
+      { checkAttribute: 'frequency', method: this.defineSimpleFeature.bind(this), featureType: Feature.types.frequency },
+      { checkAttribute: 'radical', method: this.defineSimpleFeature.bind(this), featureType: Feature.types.radical }
+    ]
+    let features = []
+
+    featuresArr.forEach(featureConfig => {
+      let featureVal = featureConfig.method(featureConfig, rawLexeme)
+      if (featureVal) {
+        features.push(featureVal)
+      }
+    })
+    return features
+  }
+
+  defineSimpleFeature (featureConfig, rawLexeme) {
+    if (rawLexeme[featureConfig.checkAttribute]) {
+      return new Feature(featureConfig.featureType, rawLexeme[featureConfig.checkAttribute], this.languageID)
+    }
+  }
+
+  extractShortDefinitions (rawLexeme) {
+    let shortdefs = []
+    shortdefs.push(new Definition(rawLexeme.shortDef, 'eng', 'text/plain', rawLexeme.word))
+  }
+}
+
+export default AlpheiosChineseLocAdapter
---
+++ tests/chinese/chinese.test.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+/* eslint-disable no-unused-vars */
+import 'whatwg-fetch'
+
+import SIMPIDX from './json/simp-idx.json'
+import TRADIDX from './json/trad-idx.json'
+import ADSODAT from './json/adsolines.json'
+import HANZIDAT from './json/hanzi-dat.json'
+
+import ChineseHelp from './chinese-help.js'
+
+import AlpheiosChineseLocAdapter from '@/adapters/chineseloc/adapter.js'
+
+describe('chinese.test.js', () => {
+  let dWordIndexSimp, dWordIndexTrad, dWordDict, dHanziDict
+
+  function timeNow () {
+    return ((this.getHours() < 10) ? '0' : '') + this.getHours() + ':' + ((this.getMinutes() < 10) ? '0' : '') + this.getMinutes() + ':' + ((this.getSeconds() < 10) ? '0' : '') + this.getSeconds()
+  }
+
+  let newDate
+
+  beforeAll(async () => {
+    dWordIndexSimp = ChineseHelp.convertIDX(SIMPIDX)
+    dWordIndexTrad = ChineseHelp.convertIDX(TRADIDX)
+    dWordDict = ChineseHelp.convertAdso2(ADSODAT)
+
+    dHanziDict = ChineseHelp.convertHanzi(HANZIDAT)
+  })
+
+  it('Chinese test - lookup prototype', () => {
+    // const targetWord = '一夫多妻主义者'
+
+    const targetWord = '阿摩尼亚'
+    // console.info('test dHanziDict - ', dHanziDict)
+    // const result = ChineseHelp.lookupChinese(targetWord, dWordIndexSimp, dWordIndexTrad, dWordDict, dHanziDict)
+    // console.info('final', result)
+
+    const adapter = new AlpheiosChineseLocAdapter()
+
+    adapter.fetchChineseData = (targetWord) => ChineseHelp.lookupChinese(targetWord, dWordIndexSimp, dWordIndexTrad, dWordDict, dHanziDict)
+    let result = adapter.getHomonym(targetWord)
+    console.info('adapter', adapter.errors)
+    console.info('result', result)
+    
+  })
+})
